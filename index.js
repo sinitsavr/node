75d767caaeb0679c2ad05018d49ee4ea363163d8
Vadim Sinitsa <sinitsavr@gmail.com>
2022-07-07 17:02:05 +0300

    example fs

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-'use strict';
+"use strict";
 
-const {MyMath} = require('./MyMath');
-const {Component} = require('./Component');
+const { MyMath } = require("./MyMath");
+const { Component } = require("./Component");
 
-const res1 = MyMath.sum(3,5);
-const res2 = MyMath.sub(3,5);
-console.log('res1=', res1);
-console.log('hi!');
+const fs = require('fs/promises');
 
+// const res1 = MyMath.sum(3,5);
+// const res2 = MyMath.sub(3,5);
+// console.log('res1=', res1);
+// console.log('hi!');
+fs.readFile('./text.txt', 'utf-8')
+.then((content) =>{
+  console.log(content)
+  fs.writeFile('./newText.txt', content + "\n1232323223", "utf-8");
+})
+.catch((err) => {console.log(err)})
+// console.log(fs)
 
-const component1 = new Component();
-component1.render();
+// const component1 = new Component();
+// component1.render();
